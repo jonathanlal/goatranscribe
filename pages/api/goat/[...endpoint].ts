8d463e7d67fefedd1fd1e6692f03f6b0f5933c6b
jonathanlal <jonathanmlaliberte@gmail.com>
2023-04-06 22:28:24 +0200

    error fixed jesus

--- pages/api/goat/[...endpoint].ts
+++ pages/api/goat/[...endpoint].ts
@@ -9,17 +9,11 @@ export default withApiAuthRequired(async function (
 
   const { accessToken } = await getAccessToken(req, res);
   const requestHeaders = new Headers();
-  // Object.entries(req.headers).forEach(([key, value]) => {
-  //   if (typeof value === 'string') {
-  //     requestHeaders.set(key, value);
-  //   }
-  // });
 
   // Set the Authorization header with the access token
   requestHeaders.set('Authorization', `Bearer ${accessToken}`);
   requestHeaders.set('Content-Type', 'application/json');
 
-  // console.log('api URL: ', `${process.env.GOAT_API}${endpoint}`);
   const response = await fetch(`${process.env.GOAT_API}${endpoint}`, {
     headers: requestHeaders,
     method: req.method, // Use the original request method
